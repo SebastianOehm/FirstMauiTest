9e1423d149aa7c1cfcf52cfb9fed0236510722d9
Sebastian Oehm <sebastianoehm2001@gmail.com>
2025-06-03 18:38:34 +0200

    Add Unit class as base for Umrechner

---
+++ Units.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstMauiTest
+{
+    public enum UnitType
+    {
+        Length,
+        Temperature
+    }
+    class Unit
+    {
+        public string Symbol { get; set; }
+        public UnitType Type { get; set; }
+        public Func<double, double>? ToBase { get; set; }
+        public Func<double, double>? FromBase { get; set; }
+    }
+}
