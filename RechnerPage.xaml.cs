070a2e41b280769225cb0102c083607b14b449a4
Sebastian Oehm <sebastianoehm2001@gmail.com>
2025-06-03 18:38:12 +0200

    Modify Rechner, use dropdown for operations/operators and add more options

--- RechnerPage.xaml.cs
+++ RechnerPage.xaml.cs
@@ -5,12 +5,22 @@ public partial class RechnerPage : ContentPage
     {
         InitializeComponent();
     }
-    private void OnAddClicked(object sender, EventArgs e)
+    private void OnCalculateClicked(object sender, EventArgs e)
     {
         if (double.TryParse(Number1Entry.Text, out double num1) &&
-            double.TryParse(Number2Entry.Text, out double num2))
+             double.TryParse(Number2Entry.Text, out double num2))
         {
-            double result = num1 + num2;
+            string op = OperatorPicker.SelectedItem as string;
+            double result = op switch
+            {
+                "+" => num1 + num2,
+                "-" => num1 - num2,
+                "*" => num1 * num2,
+                "/" => num2 != 0 ? num1 / num2 : double.NaN,
+                "^" => Math.Pow(num1, num2),
+                "%" => num1 % num2,
+                _ => 0
+            };
             ResultLabel.Text = $"Ergebnis: {result}";
         }
         else
